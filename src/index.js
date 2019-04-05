663492d0bc080be0075901fba77198a7e32beae4
Martin Schoen <schoenm@cip.ifi.lmu.de>
2019-04-05 13:33:06 +0200

    Update sample apps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Controller from './js/controller'
 
 document.app = new Controller(document.getElementById('app-root'), {
-    'text': 'text',
-    'showSentence': false
+    text: 'text',
+    showSentence: false
+})
+
+document.secondApp = new Controller(document.getElementById('app-root-2'), {
+    text: 'Second app',
+    showSentence: true
 })
