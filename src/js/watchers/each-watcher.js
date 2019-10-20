e0eb31d4d0dbb0eef72e8d701d688d5cbcba0ae5
Martin Schoen <schoenm@cip.ifi.lmu.de>
2019-10-20 19:56:24 +0200

    Enable value targets on ps-each root elements
    resolves #5

--- src/js/watchers/each-watcher.js
+++ src/js/watchers/each-watcher.js
@@ -10,16 +10,19 @@ export default function (container, iteratedElement) {
         for (var i = 0; i < newArray.length; i++) {
             var clone = iteratedElement.cloneNode(true)
             var valueTargets = clone.querySelectorAll('*[' + attr.eachValueTarget + ']')
-            if (valueTargets.length === 0) {
-                clone.textContent = newArray[i]
-            } else {
+            var target = clone
+            if (valueTargets.length > 0) {
                 if (valueTargets.length > 1) {
                     console.warn('Found more than 1 value targets in iterated element', 
                         iteratedElement, 'Using the first one')
                 }
-                var target = valueTargets[0]
-                var targetAttribute = target.getAttribute(attr.eachValueTarget) || 'textContent'
+                target = valueTargets[0]
+            }
+            var targetAttribute = target.getAttribute(attr.eachValueTarget)
+            if (targetAttribute) {
                 target.setAttribute(targetAttribute, newArray[i])
+            } else {
+                target.textContent = newArray[i]
             }
             container.appendChild(clone)
         }
